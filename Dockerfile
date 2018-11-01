6635a52ad95c1bc43ddb3338b568a62f5b3f6952
Francois Raubenheimer <fran@silvertree.holdings>
2018-11-01 10:16:17 +0200

    Docker gen developer ssl research

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+FROM ubuntu:18.10
+
+RUN apt update
+RUN apt install -y wget libssl-dev bash openssl
+
+RUN mkdir /app
+WORKDIR /app
+
+ENV DOCKER_GEN_VERSION 0.7.4
+
+RUN wget https://github.com/jwilder/docker-gen/releases/download/$DOCKER_GEN_VERSION/docker-gen-linux-amd64-$DOCKER_GEN_VERSION.tar.gz \
+ && tar -C /usr/local/bin -xvzf docker-gen-linux-amd64-$DOCKER_GEN_VERSION.tar.gz \
+ && rm docker-gen-linux-amd64-$DOCKER_GEN_VERSION.tar.gz
+
+ADD . /app
+
+ENV DOCKER_HOST unix:///var/run/docker.sock
+
+ENTRYPOINT ["/app/docker-entrypoint.sh"]
+
+CMD docker-gen -interval 10 -watch -notify  "bash /app/generator.sh" /app/generator.tmpl /app/generator.sh
---
+++ docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+
+# Warn if the DOCKER_HOST socket does not exist
+if [[ $DOCKER_HOST = unix://* ]]; then
+	socket_file=${DOCKER_HOST#unix://}
+	if ! [ -S ${socket_file} ]; then
+		cat >&2 <<-EOT
+			ERROR: you need to share your Docker host socket with a volume at $socket_file
+			Typically you should run your containers with with: \`-v /var/run/docker.sock:${socket_file}:ro\`
+		EOT
+		socketMissing=1
+	fi
+fi
+
+
+if [[ ! -d /app/ssl ]]; then
+    mkdir /app/ssl
+fi
+
+if [[ ! -d /app/ssl/root ]]; then
+    mkdir /app/ssl/root
+fi
+
+/app/generate-dhparam.sh $DHPARAM_BITS
+
+# If the user has run the default command and the socket doesn't exist, fail
+if [ "$socketMissing" = 1 -a "$1" = forego -a "$2" = start -a "$3" = '-r' ]; then
+	exit 1
+fi
+
+exec "$@"
